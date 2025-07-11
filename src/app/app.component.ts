95c01044b8d829a2803159f6e8a9c15ad5c9d7a2
Bini Babu <bini.babu@dbizsolution.com>
2025-07-11 11:26:35 +0530

    formgroup and formcontrol

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: false,
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'formgroup';
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+  });
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Login form valid', this.loginForm.value);
+    } else {
+      console.log('login Form not valid');
+    }
+  }
 }
